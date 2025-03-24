cb39edb7f6428bcd159d2b191e968d31658f634b
ccjt <circjit@gmail.com>
2025-03-24 18:21:06 -0300

    Update eval.js
    added function to catch if output is too long to send

--- eval.js
+++ eval.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         eval
 // @namespace    http://tampermonkey.net/
-// @version      v1.0-alpha1.1
+// @version      v1.0-alpha1.2
 // @description  uate
-// @author       circjit
+// @author       ccjt
 // @match        https://multiplayerpiano.org/*
 // @match        https://multiplayerpiano.net/*
 // @icon         data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAACXBIWXMAAAsSAAALEgHS3X78AAAAAXNSR0IArs4c6QAAAUhJREFUeF7tmskOwyAMROH/P5qKAxVCpB4kK7LLyy0pYpnMgklrKaWVi68KADAACeABF3tgwQRJAVKAFCAFSAEDgdZsktTaAyXfJcUgAMAAJGCKGw/ABEkBUyYRGxCDXtVg1r2CxACFugCQdLMEA/AApxMhPAAPyFkwYYKKCb6pb2UsZV/S2ygVqsQAZVLKYMrElbGUfgAABiABPEDxJUyQGBRqASWaFLop8aWMpfTjGoPqgB7trgfAA8STPiQTPOkwW1sAUFIg21s9mW84Bswm2JOl388Jc3pvgRESAHXBA4wZlBWglACMSQ8gdgvtbVa2jGcne5LwDJgX9fSm/54Buzf75AU7VvySQTgGWJr1/h0A2AcI1aA37SL1hwSQABLg7/L2V81IruU8F0wQE8QE7zDBtWL8lty3SGA9Uxj3V5jg7rxgPPsAQdQYDUupC18AAAAASUVORK5CYII=
@@ -28,17 +28,25 @@ MPP.client.on("a", function(msg) {
         if (cmd == '.') {
             function run(code) {
                 if (new String(code) == "[object Object]" == false && new String(code) == "[object JSON]" == false) {
-                    try {
-                        return '⧔ ' + eval(code);
-                    } catch (error) {
-                        return 'nope'
+                    if (JSON.stringify(eval(code)).length > 511) {
+                        return 'too long!! >//<'
+                    } else {
+                        try {
+                            return '⧔ ' + eval(code);
+                        } catch (error) {
+                            return 'nope'
+                        }
                     }
                 }
                 if (new String(code) == "[object Object]" || new String(code) == "[object JSON]") {
-                    try {
-                        return '⧔ ' + JSON.stringify(eval(code));
-                    } catch (error) {
-                        return 'nuh uh'
+                    if (JSON.stringify(eval(code)).length > 511) {
+                        return 'too long!! >//<'
+                    } else {
+                        try {
+                            return '⧔ ' + JSON.stringify(eval(code));
+                        } catch (error) {
+                            return 'nuh uh'
+                        }
                     }
                 }
             }
